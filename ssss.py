dd9c2f6e1b325abe69d66f71a7dd089cc67fbf3f
jinwoo5773-ux <jinwoo5773@gmail.com>
2026-08-28 16:49:16 +0900

    adad

--- ssss.py
+++ ssss.py
@@ -1,56 +1,37 @@
-from ultralytics import YOLO
-import cv2
-import time
+import subprocess
 
 
-model = YOLO("src/models/YOLO/yolo11n_int8.engine")
+PIPER_PYTHON = ".piper_venv/bin/python"
+PIPER_MODEL = "src/models/Piper/ko_KR-kss-medium.onnx"
+OUTPUT_FILE = "src/audio/response.wav"
+SPEAKER_DEVICE = "plughw:2,0"
 
-pipeline = (
-    "nvarguscamerasrc sensor-id=0 ! "
-    "video/x-raw(memory:NVMM), width=1280, height=720, framerate=30/1 ! "
-    "nvvidconv ! "
-    "video/x-raw, format=BGRx ! "
-    "videoconvert ! "
-    "video/x-raw, format=BGR ! "
-    "queue leaky=downstream max-size-buffers=1 ! "
-    "appsink drop=true max-buffers=1 sync=false"
+
+text = "현재는 빨간불입니다. 주의하시길 바랍니다!"
+
+subprocess.run(
+    [
+        PIPER_PYTHON,
+        "-m",
+        "piper",
+
+        "-m",
+        PIPER_MODEL,
+
+        "-f",
+        OUTPUT_FILE,
+
+        "--",
+        text,
+    ],
+    check=True,
 )
 
-cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
-
-displayed_fps = 0.0
-
-while True:
-    start_time = time.perf_counter()
-
-    ret, frame = cap.read()
-
-    if not ret:
-        break
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-    results = model.predict(
-        source=frame,   # source image
-        conf=0.25,      # Confidence Threshold
-        iou=0.5,        # IoU Threshold
-        verbose=False,  # no output prints
-        classes=None,   # selected class
-    )
-
-    output_frame = results[0].plot()
-
-    elapsed_time = time.perf_counter() - start_time
-    current_fps = 1.0 / elapsed_time
-
-    if displayed_fps == 0:
-        displayed_fps = current_fps
-    else:
-        displayed_fps = 0.9 * displayed_fps + 0.1 * current_fps
-
-    cv2.putText(output_frame, f"FPS: {displayed_fps:.1f}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
-
-    cv2.imshow("YOLO Object Detection with FPS", output_frame)
-
-cap.release()
-cv2.destroyAllWindows()
+subprocess.run(
+    [
+        "aplay",
+        "-D",
+        SPEAKER_DEVICE,
+        OUTPUT_FILE,
+    ],
+    check=True,
